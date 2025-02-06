46f4c121cd7d85d976e679505101be26733e2fa6
Amritendu Nath <amritendunath@yahoo.com>
2025-02-06 11:31:51 +0530

    Add: func_startConversation

--- frontend/src/AssistantService.js
+++ frontend/src/AssistantService.js
@@ -1,4 +1,28 @@
-class assistantService{
+import axios from 'axios'
+
+const BASE_URL = process.env.BASE_URL
+
+class assistantService {
+
+    static async startConversation() {
+        try {
+            const response = await axios({
+                url: `${BASE_URL}/graph/start`,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (response.status >= 200 && response.status < 300) {
+                return response.data
+            }
+            else {
+                console.error("Server responded with error", response.status, response.data)
+            }
+        } catch (error) {
+            console.log("Error starting conversations",error)
+        }
+    }
 
 }
 
