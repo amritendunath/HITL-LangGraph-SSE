8e530cc5643230751ce9e00d2b7a3ce9bede9fb7
Amritendu Nath <amritendunath@yahoo.com>
2025-02-06 10:54:16 +0530

    updating

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import App from './App';
+=======
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+>>>>>>> a7ba5d8 (creating arch_folder)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +14,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+<<<<<<< HEAD
+=======
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> a7ba5d8 (creating arch_folder)
