51ba39b70f731ba451eb71944d290e24b070ddc0
Amritendu Nath <amritendunath@yahoo.com>
2025-02-06 10:55:04 +0530

    updating

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import App from './App';
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
->>>>>>> a7ba5d8 (creating arch_folder)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-<<<<<<< HEAD
-=======
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> a7ba5d8 (creating arch_folder)
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
