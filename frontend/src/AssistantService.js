a9b6835b1f3951f4c72e0bdf2c2619ffbc2a1b0d
Amritendu Nath <amritendunath@yahoo.com>
2025-02-07 11:43:19 +0530

    Add: func_startConversation

--- frontend/src/AssistantService.js
+++ frontend/src/AssistantService.js
@@ -3,27 +3,51 @@ import axios from 'axios'
 const BASE_URL = process.env.BASE_URL
 
 class assistantService {
-
-    static async startConversation() {
+    //API Methods: REST
+    static async startConversation(human_req) {
         try {
             const response = await axios({
                 url: `${BASE_URL}/graph/start`,
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
-                }
+                },
+                body: JSON.stringify(human_req)
             })
             if (response.status >= 200 && response.status < 300) {
-                return response.data
+                return response.json() | response.data
             }
             else {
                 console.error("Server responded with error", response.status, response.data)
             }
         } catch (error) {
-            console.log("Error starting conversations",error)
+            console.log("Error starting conversations", error)
         }
     }
 
+    // static async submitReview({
+
+    // })
+    //API Methods: SSE
+    static async createStreamingConversation(human_req){
+        try {
+            const response = await axios({
+                url: `${BASE_URL}/graph/stream/create`,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(human_req)
+            })
+            if (response.status >= 200 && response.status < 300){
+                return response.json() | response.data
+            }
+            else{
+                console.error("Server responded with error", response.status, response.data)
+            }
+        } catch (error) {
+            console.log("Error from server side", error)
+        }
+    }
 }
 
 export default assistantService
