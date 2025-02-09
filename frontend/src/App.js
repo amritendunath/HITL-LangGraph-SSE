5cdab6a3f336f7d89193935bb57e1107fefc4a5b
Amritendu Nath <amritendunath@yahoo.com>
2025-02-09 14:11:19 +0530

    Add: README.md

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,18 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import assistantService from './AssistantService'
 import ReactMarkdown from "react-markdown";
+
+
+const App = () => {
+    const [uiState, setUiState] = useState("idle")
+    const [question, setQuestion] = useState("");
+    const [assistantResponse, setAssistantResponse] = useState("");
+    const [feedback, setFeedback] = useState("");
+    const [threadId, setThreadId] = useState(null);
+    const [history, setHistory] = useState([]);
+
+
+
+}
+
+export default App
