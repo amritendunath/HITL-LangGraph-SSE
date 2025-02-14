3076ed18df1826c41f91696425668d188b793572
Amritendu Nath <amritendunath@yahoo.com>
2025-02-14 19:20:59 +0530

    Update: endpoints

--- backend/src/api/v1/endpoints.py
+++ backend/src/api/v1/endpoints.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Request, Response
 from uuid import uuid4
 from models import StartRequest, ResumeRequest, GraphResponse
 from sse_starlette import EventSourceResponse
-import json
+from graph import graph
+import json, logging
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -28,16 +30,25 @@ async def stream_graph(request: Request, thread_id: str):
     run_data = run_config[thread_id]
     config = {"configurable": {"thread_id": thread_id}}
 
-    input_state = None
+    input_state = run_data["human_req"]
 
     async def event_generator():
         init_data = json.dumps({"thread_id": thread_id})
         yield {"data": init_data}
 
         try:
-            pass
+            # pass
+            for msg, metadata in graph.astream(input_state, config):
+                if await request.is_disconnected():
+                    logger.info("DEBUG : Client disconnected")
+                if metadata.get('langgraph_node') in ['assistant_draft']:
+                    token_data = json.dumps({"content": msg.content})
+                    logger.info(f"DEBUG: Sending token event with data: {token_data[:30]}")
+                    yield {"event": "token", "data": token_data}
 
         except Exception as e:
-            print()
+            logger.error()
             yield {"event": "error", "data": json.dumps({"error": str(e)})}
+    
+    return EventSourceResponse(event_generator())
             
